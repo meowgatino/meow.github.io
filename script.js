957ccd7249bd9289ce425865d3e9dcba9f49e094
meowgatino <kirkdejesusg@gmail.com>
2024-10-30 23:28:30 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const btnNo = document.querySelector("#random-btn")
+
+function moverAleatoriamente(btn) {
+    btn.style.position = "absolute";
+    btn.style.fontWeight = "bolder";
+    btn.style.top = Math.floor(Math.random() * 90 + 5) + "%"
+    btn.style.left = Math.floor(Math.random() * 90 + 5) + "%"
+}
+
+btnNo.addEventListener("mouseenter", function (e) {
+    moverAleatoriamente(e.target)
+})
